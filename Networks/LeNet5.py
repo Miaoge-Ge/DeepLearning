cfb20ddf1fbdabcd921b619ffd243bfb019235ee
JJM <124609700+Miaoge-Ge@users.noreply.github.com>
2025-07-27 16:52:52 +0800

    Add files via upload

---
+++ Networks/LeNet5.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+
+
+class LeNet5(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(16 * 4 * 4, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
